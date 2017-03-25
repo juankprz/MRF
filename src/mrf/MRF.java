b9fa9dc6b10845c132e37e72ffb330c3e80a3fdd
JUAN <JUAN@JUAN-PC.home>
2017-03-25 14:54:33 -0500

    Restructuracion del codigo "mejora de constructor "

--- src/mrf/MRF.java
+++ src/mrf/MRF.java
@@ -14,7 +14,20 @@ import net.objecthunter.exp4j.ExpressionBuilder;
  * @author JUAN
  */
 public class MRF {
-       public MRF(){}
+    String funcion;
+    double inicial;
+    double fin;
+    int iteration;
+    double error;
+
+    public MRF(String funcion, double inicial, double fin, int iteration, double error) {
+        this.funcion = funcion;
+        this.inicial = inicial;
+        this.fin = fin;
+        this.iteration = iteration;
+        this.error = error;
+    }
+       
     
     /**
      * Función a resolver
@@ -23,7 +36,7 @@ public class MRF {
      */
     public double fx(double x){
                //x^3 - 2x^2 + 3x -8 
-               String funcion="x^3 - 2x^2 + 3x -8";
+               String funcion="x^3 - 2x^2 + 3x +54";
            net.objecthunter.exp4j.Expression e = new ExpressionBuilder(funcion)
                 .variables("x")
                 .build()
@@ -43,32 +56,32 @@ public class MRF {
      *                EMPTY en caso de no hallar convergencia al limite de iteraciones permitidas
      */
     
-    public String resolver(double x1, double x2, int iterations, double err){
+    public String resolver(){
         
         int numDec=5;
         double x3, y3;
-        double xp=x1;
-        double y1=fx(x1);
-        double y2=fx(x2);   
+        double xp=inicial;
+        double y1=fx(inicial);
+        double y2=fx(fin);   
+        int iteraciones=iteration;
         
         
         
-        for(int i=1; i<=iterations;i++){            
+        for(int i=1; i<=iteraciones;i++){            
             System.out.println("Iteración N°"+i);
             if ( (y2-y1) == 0 ){
                System.err.println("Error: no converge x=NaN; iteración="+i);
                return null;
             }
             
-            x3=(y2*x1-y1*x2)/(y2-y1);
+            x3=(y2*inicial-y1*fin)/(y2-y1);
             System.out.println(" x3 = (y2*x1-y1*x2)/(y2-y1)");
-            System.out.println(" x3 = ((" + round(y2,numDec)+")*("+round(x1,numDec)+")-("+round(y1,numDec)+")*("+round(x2,numDec)+"))/(("+round(y2,numDec)+")-("+round(y1,numDec)+")) = " + round(x3,numDec) );
-            
-            System.out.println("Error |"+round(xp,numDec)+"/"+round(x3,numDec)+"-1|<"+round(err,numDec)  + "  " + (Math.abs(xp/x3-1)<err));
+            System.out.println(" x3 = ((" + round(y2,numDec)+")*("+round(inicial,numDec)+")-("+round(y1,numDec)+")*("+round(fin,numDec)+"))/(("+round(y2,numDec)+")-("+round(y1,numDec)+")) = " + round(x3,numDec) );         
+            System.out.println("Error |"+round(xp,numDec)+"/"+round(x3,numDec)+"-1|<"+round(error,numDec)  + "  " + (Math.abs(xp/x3-1)<error));
             System.out.println("Error |"+Math.abs(xp/x3-1)+"|");
              System.out.println("-----------------------------------------------------------------");
-            if (Math.abs(xp/x3-1)<err){                
-                System.out.println(round(Math.abs(xp/x3-1),numDec)+"<"+round(err,numDec) + " -> termina programa");
+            if (Math.abs(xp/x3-1)<error){                
+                System.out.println(round(Math.abs(xp/x3-1),numDec)+"<"+round(error,numDec) + " -> termina programa");
                 return "Iteración:" + i + " Valor x=" + String.valueOf(x3);
               
             }
@@ -81,12 +94,12 @@ public class MRF {
             if (y1*y3<0) {
                 System.out.println("   x2=x3=" + round(x3,numDec));
                 System.out.println("   y2=y3=" + round(y3,numDec));
-                x2=x3;
+                fin=x3;
                 y2=y3;
             } else {
                 System.out.println("   x1=x3=" + round(x3,numDec));
                 System.out.println("   y1=y3=" + round(y3,numDec));
-                x1=x3;
+                inicial=x3;
                 y1=y3;
             }
             xp=x3;
@@ -108,8 +121,10 @@ public class MRF {
     }
         
     public static void main(String[] args){
-        MRF mrf = new MRF();
-        String res = mrf.resolver(3,4,50,0.01);
+        String Funcion="";
+        
+        MRF mrf = new MRF(Funcion,-6,-2,10,0.01);
+        String res = mrf.resolver();
         System.out.println(res);
     }
 }
